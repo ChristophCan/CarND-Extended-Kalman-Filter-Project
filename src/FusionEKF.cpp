786c7236912a4ae06b4936f6f5b22786d32e519e
Christoph Chalfin <christoph.chalfin@posteo.de>
2018-12-02 12:54:51 +0100

    Update process for Lidar measurements implemented

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -149,8 +149,19 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
     // Radar updates
+
+    //Mesurement Covariance for Radar
+    ekf_.R_ = R_radar_;
   } else {
     // Laser updates
+    
+    //Measurement Covariance for Lidar
+    ekf_.R_ = R_laser_;
+
+    //Measurement Matrix for Lidar
+    ekf_.H_ = H_laser_;
+
+    ekf_.Update(measurement_pack.raw_measurements_);
   }
 
   // print the output
